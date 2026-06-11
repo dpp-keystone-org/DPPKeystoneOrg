0c4d63d3f890789d7c086f325c2900add07f1e68
Michael Linck <mlinck@google.com>
2026-06-11 13:38:28 +0000

    try to fix array and optional object buttons

--- src/wizard/form-builder.js
+++ src/wizard/form-builder.js
@@ -626,6 +626,7 @@ function renderArrayProperty(fragment, { prop, currentPath, indentationLevel, on
                 insertionPoint = allItemControls[allItemControls.length - 1];
             }
             insertionPoint.after(newObjectFragment);
+            triggerLocalization();
 
             triggerValidationForGroup(insertionPoint, newObjectPath);
         });
@@ -898,6 +899,7 @@ function createOptionalObjectPlaceholderRow(key, prop, currentPath, indentationL
 
         // Insert the new rows after the transformed header row.
         placeholderRow.after(newFieldsFragment);
+        triggerLocalization();
 
         // 5z-h: Now that the new elements are in the DOM, find their inputs and trigger validation.
         const newInputs = placeholderRow.parentElement.querySelectorAll(`[data-optional-object-groups~="${key}"] input:not([type="checkbox"]), [data-optional-object-groups~="${key}"] select`);
